1e924a5c68a3a4ed4cd22e9042a22d36ab5336fd
tarun.murala <tarun.murala@servicenow.com>
2019-09-18 16:42:59 -0700

    Including maven

--- vars/build-test.groovy
+++ vars/build-test.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 def build() {
     stage('build') {
-        sh 'mvn clean package -DskipTests=true'
+        sh '${mvnHome}/bin/mvn clean package -DskipTests=true'
     }
 }
 def test() {
     stage ('test') {
-        sh 'mvn test -Dpublish'
+        sh '${mvnHome}/bin/mvn test -Dpublish'
     }
 }
 
